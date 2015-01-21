6233bf226589ba0e9d3238b55f933d1420f47282
Patrick Mueller <pmuellr@apache.org>
2015-01-21 09:15:29 -0500

    tabs to spaces

--- cf-plugin-echo.go
+++ cf-plugin-echo.go
@@ -10,70 +10,70 @@ package main
 
 //------------------------------------------------------------------------------
 import (
-	"flag"
-	"fmt"
-	"os"
-	"strings"
+  "flag"
+  "fmt"
+  "os"
+  "strings"
 
-	pluginAPI "github.com/cloudfoundry/cli/plugin"
+  pluginAPI "github.com/cloudfoundry/cli/plugin"
 )
 
 //------------------------------------------------------------------------------
 type Plugin struct {
-	uppercase *bool
+  uppercase *bool
 }
 
 //------------------------------------------------------------------------------
 func main() {
-	pluginAPI.Start(new(Plugin))
+  pluginAPI.Start(new(Plugin))
 }
 
 //------------------------------------------------------------------------------
 func (plugin *Plugin) Run(cliConn pluginAPI.CliConnection, args []string) {
-	// Initialize flags
-	argsOpts  := flag.NewFlagSet("echo", flag.ExitOnError)
-	uppercase := argsOpts.Bool("uppercase", false, "displays all provided text in uppercase")
+  // Initialize flags
+  argsOpts  := flag.NewFlagSet("echo", flag.ExitOnError)
+  uppercase := argsOpts.Bool("uppercase", false, "displays all provided text in uppercase")
 
-	// Parse starting from [1] because the [0]th element is the name of the command
-	err := argsOpts.Parse(args[1:])
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
+  // Parse starting from [1] because the [0]th element is the name of the command
+  err := argsOpts.Parse(args[1:])
+  if err != nil {
+    fmt.Println(err)
+    os.Exit(1)
+  }
 
-	var result string
-	for _, value := range argsOpts.Args() {
-		if *uppercase {
-			result += strings.ToUpper(value) + " "
-		} else {
-			result += value + " "
-		}
-	}
+  var result string
+  for _, value := range argsOpts.Args() {
+    if *uppercase {
+      result += strings.ToUpper(value) + " "
+    } else {
+      result += value + " "
+    }
+  }
 
-	fmt.Println(result)
+  fmt.Println(result)
 }
 
 //------------------------------------------------------------------------------
 func (plugin *Plugin) GetMetadata() pluginAPI.PluginMetadata {
-	return pluginAPI.PluginMetadata{
-		Name: "echo",
-		Version: pluginAPI.VersionType{
-			Major: 0,
-			Minor: 1,
-			Build: 0,
-		},
-		Commands: []pluginAPI.Command{
-			{
-				Name:     "echo",
-				Alias:    "echo-what-is-passed-in",
-				HelpText: "Echo text passed into the command. To obtain more information use --help",
-				UsageDetails: pluginAPI.Usage{
-					Usage: "echo - print input arguments to screen\n   cf echo [-uppercase] text",
-					Options: map[string]string{
-						"uppercase": "If this param is passed, which ever word is passed to echo will be all capitals.",
-					},
-				},
-			},
-		},
-	}
+  return pluginAPI.PluginMetadata{
+    Name: "echo",
+    Version: pluginAPI.VersionType{
+      Major: 0,
+      Minor: 1,
+      Build: 0,
+    },
+    Commands: []pluginAPI.Command{
+      {
+        Name:     "echo",
+        Alias:    "echo-what-is-passed-in",
+        HelpText: "Echo text passed into the command. To obtain more information use --help",
+        UsageDetails: pluginAPI.Usage{
+          Usage: "echo - print input arguments to screen\n   cf echo [-uppercase] text",
+          Options: map[string]string{
+            "uppercase": "If this param is passed, which ever word is passed to echo will be all capitals.",
+          },
+        },
+      },
+    },
+  }
 }
